6383b96eba95fa2c918c1047b801488df9285273
Joseph Billings <josephbillings.gb@gmail.com>
2024-06-24 15:41:46 -0600

    created back of card by using .map to cycle through cards and deconstruct the object stats to be displayed in p tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
+import playerData from "./playerData.js";
+import { useState } from "react";
+
 function BaseballCard(props) {
+  const statsDisplay = Object.entries(props.stats).map(
+    ([statName, statValue]) => (
+      <p key={statName}>
+        {statName}: {statValue}
+      </p>
+    )
+  );
+
   return (
     <div className="card">
-      <h2>Player Name goes here</h2>
+      <h2>{props.name}</h2>
+      <p>Team: {props.team}</p>
+      <p>Position: {props.position}</p>
+      {statsDisplay}
     </div>
   );
+  // return (
+  //   <div className="card">
+  //     <h2>{props.name}</h2>
+  //     <img src={props.imgURL} alt={props.name} />
+  //   </div>
+  // );
 }
 
 function App() {
-  return <BaseballCard />;
+  const cards = playerData.map((player) => (
+    <BaseballCard
+      name={player.name}
+      team={player.team}
+      position={player.position}
+      stats={player.stats}
+      imgURL={player.imgUrl}
+      key={player.cardId}
+    />
+  ));
+  return <>{cards}</>;
 }
 
 export default App;
