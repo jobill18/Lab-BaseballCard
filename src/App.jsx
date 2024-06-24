d902721cb8569d980508d725557f2c27944a435b
Joseph Billings <josephbillings.gb@gmail.com>
2024-06-24 15:58:40 -0600

    created toggle function to change setShowPicture to true or false and added onClick to the div tags in the two return states for BaseballCards

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import { useState } from "react";
 
 function BaseballCard(props) {
   const [showPicture, setShowPicture] = useState(true);
+  function toggleCard() {
+    if (!showPicture) {
+      setShowPicture(true);
+    } else {
+      setShowPicture(false);
+    }
+  }
+
   const statsDisplay = Object.entries(props.stats).map(
     ([statName, statValue]) => (
       <p key={statName}>
@@ -12,14 +20,14 @@ function BaseballCard(props) {
   );
   if (showPicture) {
     return (
-      <div className="card">
+      <div className="card" onClick={toggleCard}>
         <h2>{props.name}</h2>
         <img src={props.imgURL} alt={props.name} />
       </div>
     );
   } else {
     return (
-      <div className="card">
+      <div className="card" onClick={toggleCard}>
         <h2>{props.name}</h2>
         <p>Team: {props.team}</p>
         <p>Position: {props.position}</p>
