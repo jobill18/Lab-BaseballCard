2a865d719ea50c7295bdc5e7e882b1dd3ebe4e15
Joseph Billings <josephbillings.gb@gmail.com>
2024-06-24 15:48:55 -0600

    added State for showPicture and logic to display the front or back of the card backed on the state, setup for event handler

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import playerData from "./playerData.js";
 import { useState } from "react";
 
 function BaseballCard(props) {
+  const [showPicture, setShowPicture] = useState(true);
   const statsDisplay = Object.entries(props.stats).map(
     ([statName, statValue]) => (
       <p key={statName}>
@@ -9,21 +10,23 @@ function BaseballCard(props) {
       </p>
     )
   );
-
-  return (
-    <div className="card">
-      <h2>{props.name}</h2>
-      <p>Team: {props.team}</p>
-      <p>Position: {props.position}</p>
-      {statsDisplay}
-    </div>
-  );
-  // return (
-  //   <div className="card">
-  //     <h2>{props.name}</h2>
-  //     <img src={props.imgURL} alt={props.name} />
-  //   </div>
-  // );
+  if (showPicture) {
+    return (
+      <div className="card">
+        <h2>{props.name}</h2>
+        <img src={props.imgURL} alt={props.name} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="card">
+        <h2>{props.name}</h2>
+        <p>Team: {props.team}</p>
+        <p>Position: {props.position}</p>
+        {statsDisplay}
+      </div>
+    );
+  }
 }
 
 function App() {
